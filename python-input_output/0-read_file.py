82abeda3271e3a9f462ef4c86d5fbd257cbc5136
Hussam <samdevy@outlook.com>
2025-12-25 08:16:44 +0000

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,9 +5,9 @@ this file is using with open so you can read files from python
 """
 def read_file(filename=""):
 
-"""  
-this file is using with open so you can read files from python
-"""
+   """  
+   this file is using with open so you can read files from python
+   """
    with open(filename, "r", encoding=utf8) as f:
 
      print(f.read())
