437a0c8aa1425f6e2ca75fdca5a79865cceb49b1
Hussam <samdevy@outlook.com>
2025-12-29 03:00:54 +0000

    Create 12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+12-pascal_triangle.py
+Create Pascal's Triangle
+"""
+
+def pascal_triangle(n):
+    """Return Pascal's Triangle as a list of lists of integers"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # First row is always [1]
+
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        row = [1]  # First element of each row is always 1
+
+        # Build the middle elements by adding adjacent elements from previous row
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)  # Last element of each row is always 1
+        triangle.append(row)
+
+    return triangle
