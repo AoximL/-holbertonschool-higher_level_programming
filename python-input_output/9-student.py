089a179e1049c667071a618ba8f31d5e6d741e6a
Hussam <samdevy@outlook.com>
2025-12-29 02:38:06 +0000

    Implement Student class with initialization and to_json method
    Defines a Student class with attributes and a method to retrieve its dictionary representation.

---
+++ python-input_output/9-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module defines a Student class.
+"""
+
+
+class Student:
+    """
+    Defines a student by first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name: The student's first name.
+            last_name: The student's last name.
+            age: The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of the Student instance.
+
+        Returns:
+            A dictionary containing the student's attributes.
+        """
+        # Return the instance dictionary
+        return self.__dict__
